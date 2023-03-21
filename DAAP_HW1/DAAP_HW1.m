aa88a0959be8106eb1a8f37a5ed806a04c642082
pox17 <paos9904@gmail.com>
2023-03-21 13:19:23 +0100

    Startup of SASP homework

---
+++ DAAP_HW1/DAAP_HW1.m
@@ -0,0 +1,15 @@
+%==========================================================================
+%                           DAAP HW1
+%                           
+%==========================================================================
+
+instr_name = "piano.wav";
+speech_name = "speech.wav"
+
+
+
+
+[instr_t,instr_Fs] = audioread(instr_name);
+[speech_t,speech_Fs] = audioread(speech_name);
+
+
