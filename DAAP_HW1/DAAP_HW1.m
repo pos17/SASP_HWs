75055374afb22a14a55c0fc78c926900028788ad
pox17 <paos9904@gmail.com>
2023-03-28 11:04:43 +0200

    uploading from a safe point

--- DAAP_HW1/DAAP_HW1.m
+++ DAAP_HW1/DAAP_HW1.m
@@ -1,15 +1,19 @@
+clc; close all; clear all;
+
 %==========================================================================
 %                           DAAP HW1
 %                           
 %==========================================================================
 
-% xcorr, circshift, cell
+% xcorr, circshift, cell, window
 
 instr_name = "piano.wav";
 instr_name_recon = "piano_recon.wav";
 speech_name = "speech.wav";
 st_res_name = "whitening_filter_res.wav";
-
+st_res_name_test = "whitening_filter_res_test.wav";
+st_res_sub_name = "subtracted_res.wav";
+st_res_sub_name_test = "subtracted_res.wav";
 
 
 [instr_t,instr_Fs] = audioread(instr_name);
@@ -17,25 +21,29 @@ st_res_name = "whitening_filter_res.wav";
 
 instr_t_len =length(instr_t);
 
-taps = 220;
+taps = 700;
 p = taps;
 wl =  1024;  % windowLenght =
 
-t_buckets = ceil(instr_t_len/wl);
-instr_t(end+1:wl*t_buckets) = 0;
-st_signal = reshape(instr_t,[wl t_buckets]);
+%t_buckets = ceil(instr_t_len/wl);
+%instr_t(end+1:wl*t_buckets) = 0;
+%st_signal = reshape(instr_t,[wl t_buckets]);
+[st_signal,chunksNum] = windowing(instr_t,"hamming",1024);
 
+
+t_buckets = chunksNum;
 r = zeros(p+1, t_buckets);
 
 M = wl;
 for nn = 1:t_buckets
     for ii_kk = 0:p
         for mm = 1:M-(ii_kk)
-            r(ii_kk+1,nn) = r(ii_kk+1,nn)+ st_signal(mm)*st_signal(mm+ii_kk);
+            r(ii_kk+1,nn) = r(ii_kk+1,nn)+ st_signal(mm,nn)*st_signal(mm+ii_kk,nn);
         end
     end 
 end
 r_0 = r(1:end-1,:);
+figure
 plot(r_0);
 r_1 = r(2:end,:);
 R = zeros(p,p,t_buckets);
@@ -49,39 +57,40 @@ end
 R_1 = R(:,:,1);
 %% Reconstruction of the prediction using prediction filters 
 
-reconstructed_Sig = zeros(wl, t_buckets);
+%reconstructed_Sig = zeros(wl, t_buckets);
 
-for nn = 1:t_buckets
-    for mm = p+1:M
-        for kk = 1:p
-            reconstructed_Sig(mm,nn) = reconstructed_Sig(mm,nn) ...
-                + a(kk,nn)*st_signal(mm-kk+1,nn);
-        end
-    end
-end
-reconstructed_Sig = reshape(reconstructed_Sig,[t_buckets *wl 1]);
-
-audiowrite(instr_name_recon,reconstructed_Sig,instr_Fs);
+%for nn = 1:t_buckets
+%    for mm = p+1:M
+%        for kk = 1:p
+%            reconstructed_Sig(mm,nn) = reconstructed_Sig(mm,nn) ...
+%                + a(kk,nn)*st_signal(mm-kk+1,nn);
+%        end
+%    end
+%end
+%reconstructed_Sig = reshape(reconstructed_Sig,[t_buckets *wl 1]);
+%reconstructed_Sig = adding(st_)
+%audiowrite(instr_name_recon,reconstructed_Sig,instr_Fs);
 
 %% Z_transform of the filter represented as spectrum of the filter
-n = wl;
+%n = wl;
 
-w = linspace(-Fs/2,Fs/2,n);
+%w = linspace(-instr_Fs/2,instr_Fs/2,n);
 
-A = zeros(length(w),t_buckets);
-z= exp(1i*w);
-sum_A =zeros(length(w),t_buckets);
-for nn=1:t_buckets
-    for kk = 1:p
-        sum_A(:,nn) = a(kk,nn)*z.^(-kk);
-    end
-end
-A = 1- sum_A;
-Aishift = zeros(wl,t_buckets);
-for nn = 1: t_buckets
-    Aishift(:,nn) = ifftshift(A(:,nn));
-end 
-A1 = Aishift(1:512,:);
+%A = zeros(length(w),t_buckets);
+%z= exp(1i*w);
+%sum_A =zeros(length(w),t_buckets);
+%for nn=1:t_buckets
+%    for kk = 1:p
+%        sum_A(:,nn) = a(kk,nn)*z.^(-kk);
+%    end
+%end
+%A = 1- sum_A;
+%Aishift = zeros(wl,t_buckets);
+%for nn = 1: t_buckets
+%    Aishift(:,nn) = ifftshift(A(:,nn));
+%end 
+%A1 = Aishift(1:512,:);
+
 %% STFT of the signal 
 w1   = linspace(0,instr_Fs/2,wl/2);
 st_signal_w = zeros(wl,t_buckets);
@@ -90,37 +99,115 @@ for nn = 1:t_buckets
 end
 
 Fs = instr_Fs;
-
+figure
 t =linspace(0,instr_t_len/instr_Fs,t_buckets);
 st_signal_w1 = st_signal_w(1:512,:);
 surf(t,w1,abs(st_signal_w1),EdgeColor="none");
 ylim([0 5000])
 view(0,90)
+disp(st_signal_w(1,400))
 
-%% signal filtering
 
+%% Z tranform other method
+a_ones = ones(1,t_buckets);
+a_1 = zeros(p+1,t_buckets);
+A = zeros(wl,t_buckets);
+H = zeros(wl,t_buckets);
+
+for nn = 1: t_buckets
+    a_1(:,nn) = vertcat(1, -a(:,nn));
+    
+    [A(:,nn),~] = freqz(a_1(:,nn),1,"whole",wl);
+    A(:,nn) = A(:,nn)/(mean(A(:,nn)/mean(st_signal_w(:,nn))));
+    [H(:,nn),w] = freqz(1,a_1(:,nn),"whole",wl);
+    H(:,nn) = H(:,nn)/(mean(H(:,nn))/mean(st_signal_w(:,nn)));
+    %A(:,nn) = 1/(H(:,nn));
+    
+end 
+figure
+plot(w/pi,A(:,800));
+hold on;
+plot(w/pi,H(:,800));
+
+
+%% piano signal filtering
+st_signal_w = zeros(wl,t_buckets);
+for nn = 1:t_buckets
+    st_signal_w(:,nn) = fft(st_signal(:,nn));
+end
 
 
 st_res =  zeros(wl, t_buckets); 
-st_res_w = zeros(wl,nn);
+st_res_w = zeros(wl,t_buckets);
 for nn = 1:t_buckets
-    st_res_w(:,nn) = A(:,nn) .* fftshift(st_signal_w(:,nn));
-    st_res_w(:,nn) = ifftshift(st_res_w(:,nn));
+    st_res_w(:,nn) =  st_signal_w(:,nn).* A(:,nn);
+    %st_res_w(:,nn) = st_signal_w(:,nn)./ H(:,nn);
     st_res(:,nn) = ifft(st_res_w(:,nn));
 end
 
-st_res_lin = reshape(st_res,[t_buckets *wl 1]);
+%st_res_lin = reshape(st_res,[t_buckets *wl 1]);
+st_res_lin = adding(st_res,0.5,1024);
+
 audiowrite(st_res_name,abs(st_res_lin),instr_Fs);
 
+instr_t(end+1:length(st_res_lin)) = 0;
+st_res_form = instr_t- st_res_lin;
+
+audiowrite(st_res_sub_name,abs(st_res_form),instr_Fs);
+
+
+
+
+
+%% test using matlab lpc
+
+H_test = zeros(wl,t_buckets);
+for nn = 1:t_buckets
+   a_test(:,nn) = lpc(st_signal(:,nn), p);
+   %a_1_test(:,nn) = vertcat(1, -a_test(:,nn));
+    
+    [A_test(:,nn),~] = freqz(a_test(:,nn),1,"whole",wl);
+    %A_test(:,nn) = A_test(:,nn)/(mean(A_test(:,nn)/mean(st_signal_w(:,nn))));
+    [H_test(:,nn),w] = freqz(1,a_test(:,nn),"whole",wl);
+    %H_test(:,nn) = H_test(:,nn)/(mean(H_test(:,nn))/mean(st_signal_w(:,nn)));
+    %A(:,nn) = 1/(H(:,nn));
+end
 
 %% plot of a random element of the matrix for the signal and for the filter 
-plot(w1,abs(st_signal_w1(:,400)))
+figure
+plot(w,10*log10(abs(st_signal_w(:,600)).^2))
+%plot(w,abs(st_signal_w(:,600)))
+
 hold on 
-plot(w1,abs(A1(:,400)))
-xlim([0,500])
+plot(w,10*log10(abs(H(:,600)).^2))
+%plot(w,abs(H(:,600)))
+hold on 
+plot(w,10*log10(abs(H_test(:,600)).^2),"LineStyle","--")
+%plot(w,abs(H(:,600)))
 
-%Hp = hamming(63,'periodic');
-%wvtool(Hp)
+xlim([0,1])
+legend("signal chunk","filter shape","test filter shape")
 
-%Hp = hamming(63,'symmetric');
-%wvtool(Hs)
+%% piano signal filtering test
+
+
+st_res_test =  zeros(wl, t_buckets); 
+st_res_w_test = zeros(wl,nn);
+for nn = 1:t_buckets
+    st_res_w_test(:,nn) =  st_signal_w(:,nn).* A_test(:,nn);
+    %st_res_w(:,nn) = st_signal_w(:,nn)./ H(:,nn);
+    st_res_test(:,nn) = ifft(st_res_w(:,nn));
+end
+
+%st_res_lin = reshape(st_res,[t_buckets *wl 1]);
+st_res_lin_test = adding(st_res,0.5,1024);
+
+audiowrite(st_res_name_test,abs(st_res_lin),instr_Fs);
+
+instr_t(end+1:length(st_res_lin_test)) = 0;
+st_res_form_test = instr_t- st_res_lin_test;
+
+audiowrite(st_res_sub_name_test,abs(st_res_form_test),instr_Fs);
+
+
+
---
+++ DAAP_HW1/main.m
@@ -0,0 +1,78 @@
+clc; close all; clear all;
+
+%==========================================================================
+%                           DAAP HW1 main
+%                           
+%==========================================================================
+
+% xcorr, circshift, cell, window
+
+instr_name = "piano.wav";
+instr_name_recon = "piano_recon.wav";
+speech_name = "speech.wav";
+talking_instr_res_name = "talking_instr_res.wav";
+instr_res_name = "instr_filter_res.wav";
+st_res_sub_name = "subtracted_res.wav";
+st_res_sub_name_test = "subtracted_res. wav";
+
+
+[instr_t,instr_Fs] = audioread(instr_name);
+[speech_t,speech_Fs] = audioread(speech_name);
+
+speech_t(end+1:length(instr_t),1) =0;
+instr_t_len =length(instr_t);
+
+taps_speech = 800;
+taps_music = 800;
+
+wl =  1024; 
+
+[instr_st_signal,chunksNum_instr] = windowing(instr_t,"hann",wl);
+[speech_st_signal,chunksNum_speech] = windowing(speech_t,"hann",wl);
+
+
+[instr_H,instr_A] =  myLpc(instr_st_signal,taps_music);
+[speech_H,speech_A] =  myLpc(speech_st_signal,taps_speech);
+
+
+instr_st_signal_w = zeros(wl,chunksNum_instr);
+speech_st_signal_w = zeros(wl,chunksNum_speech);
+
+for nn = 1:chunksNum_instr
+    instr_st_signal_w(:,nn) = fft(instr_st_signal(:,nn));
+    speech_st_signal_w(:,nn) = fft(speech_st_signal(:,nn));
+    
+end
+
+
+instr_st_res =  zeros(wl, chunksNum_instr); 
+instr_st_res_w = zeros(wl,chunksNum_instr);
+talking_instr_st_res =  zeros(wl, chunksNum_instr); 
+talking_instr_st_res_w = zeros(wl,chunksNum_instr);
+for nn = 1:chunksNum_instr
+    %instr_A(:,nn) = instr_A(:,nn)/(mean(instr_A(:,nn)/mean(instr_st_signal_w(:,nn))));
+    instr_st_res_w(:,nn) =  instr_st_signal_w(:,nn).* instr_A(:,nn);
+    talking_instr_st_res_w(:,nn) = instr_st_res_w(:,nn) .* speech_H(:,nn);
+    %st_res_w(:,nn) = st_signal_w(:,nn)./ H(:,nn);
+    instr_st_res(:,nn) = ifft(instr_st_res_w(:,nn));
+    talking_instr_st_res(:,nn) = ifft(talking_instr_st_res_w(:,nn));
+end
+
+% try to normalize output
+
+%for nn = 1:chunksNum_instr
+%    instr_st_res(:,nn) = instr_st_res(:,nn) / mean(instr_st_res(:,nn));
+%end
+
+
+%st_res_lin = reshape(st_res,[t_buckets *wl 1]);
+
+talking_instr_lin = adding(talking_instr_st_res,0.5,wl);
+instr_lin = adding(instr_st_res,0.5,wl);
+%st_res_lin = adding(instr_st_res,0.5,wl);
+%st_res_lin = adding(instr_st_res,0.5,wl);
+%st_res_lin = st_res_lin / mean(st_res_lin);
+%st_res_lin = adding(instr_st_res,0.5,wl);
+
+audiowrite(talking_instr_res_name,real(talking_instr_lin),instr_Fs);
+audiowrite(instr_res_name,real(instr_lin),instr_Fs);
