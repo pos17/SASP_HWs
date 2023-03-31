88a091e703da1bb99a9fd0be436f5171a16867f9
pox17 <paos9904@gmail.com>
2023-03-31 19:50:53 +0200

    implemented different window lengths

--- DAAP_HW1/HW1.m
+++ DAAP_HW1/HW1.m
@@ -12,8 +12,8 @@ talking_instr_res_name = "talking_instr_res.wav";
 %% FIRST TEST WITH VERBOSE PARAMETERS TO CHECK FUNCTIONING OF THE SCRIPT
 close all;
 tic
-talking_instr_res_name = "talking_instr_verb.wav";
-main(instr_name, speech_name, talking_instr_res_name,"linSolve",0.3,10^-1,100000,"zeros",0,0,1,"threshold 10^(-1)");
+talking_instr_res_name = "talking_instr_verb1.wav";
+main(instr_name, speech_name, talking_instr_res_name,2048,4096,44,100,"linSolve",0.3,10^-1,100000,"zeros",0,0,1,"threshold 10^(-1)","mean");
 toc
 
 %% test for various threshold values using steepest descent
@@ -21,18 +21,18 @@ close all;clc;
 disp("threshold 10^-1")
 tic
 talking_instr_res_name = "talking_instr_10_1.wav";
-main(instr_name, speech_name, talking_instr_res_name,"mixed1",0.3,10^-1,100000,"zeros",1,0,0,"threshold $10^{-1}$");
+main(instr_name, speech_name, talking_instr_res_name,2048,2048,44,100,"mixed1",0.3,10^-1,100000,"zeros",1,0,0,"threshold $10^{-1}$","mean");
 toc
 disp("threshold 10^-2")
 tic
 talking_instr_res_name = "talking_instr_10_2.wav";
-main(instr_name, speech_name, talking_instr_res_name,"mixed1",0.3,10^-2,100000,"zeros",1,0,0,"threshold $10^{-2}$");
+main(instr_name, speech_name, talking_instr_res_name,"mixed1",2048,2048,44,100,0.3,10^-2,100000,"zeros",1,0,0,"threshold $10^{-2}$","mean");
 toc
 disp("threshold 10^-4")
 
 tic
 talking_instr_res_name = "talking_instr_10_4.wav";
-main(instr_name, speech_name, talking_instr_res_name,"mixed1",0.3,10^-4,100000,"zeros",1,0,0,"threshold $10^{-4}$");
+main(instr_name, speech_name, talking_instr_res_name,"mixed1",2048,2048,44,100,0.3,10^-4,100000,"zeros",1,0,0,"threshold $10^{-4}$","mean");
 toc
 
 
@@ -42,21 +42,21 @@ disp("time for speech steepest descent equals to previous value")
 
 tic
 talking_instr_res_name = "talking_instr_zeros.wav";
-main(instr_name, speech_name, talking_instr_res_name,"mixed1",0.3,0.5*10^-1,100000,"prev",1,0,0,"prev");
+main(instr_name, speech_name, talking_instr_res_name,"mixed1",2048,2048,44,100,0.3,0.5*10^-1,100000,"prev",1,0,0,"prev","mean");
 toc
 disp("time for speech steepest descent equals to zero")
 
 
 tic
 talking_instr_res_name = "talking_instr_zeros.wav";
-main(instr_name, speech_name, talking_instr_res_name,"mixed1",0.3,0.5*10^-1,100000,"zeros",1,0,0,"zeros");
+main(instr_name, speech_name, talking_instr_res_name,"mixed1",2048,2048,44,100,0.3,0.5*10^-1,100000,"zeros",1,0,0,"zeros","mean");
 toc
 disp("time for speech steepest descent equals to one")
 
 tic
 
 talking_instr_res_name = "talking_instr_ones.wav";
-main(instr_name, speech_name, talking_instr_res_name,"mixed1",0.3,0.5*10^-1,100000,"ones",1,0,0,"ones");
+main(instr_name, speech_name, talking_instr_res_name,"mixed1",2048,2048,44,100,0.3,0.5*10^-1,100000,"ones",1,0,0,"ones","mean");
 toc
 
 disp("time for speech steepest descent equals to random values")
@@ -64,7 +64,7 @@ disp("time for speech steepest descent equals to random values")
 
 tic
 talking_instr_res_name = "talking_instr_randn.wav";
-main(instr_name, speech_name, talking_instr_res_name,"mixed1",0.3,0.5*10^-1,100000,"randn",1,0,0,"prev");
+main(instr_name, speech_name, talking_instr_res_name,"mixed1",2048,2048,44,100,0.3,0.5*10^-1,100000,"randn",1,0,0,"prev","mean");
 toc
 
 %sound(avviso(1:5*44100),44100);
