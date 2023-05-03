313d1c5b1549083377c0a87366d83d3b770d5fcb
pox17 <paos9904@gmail.com>
2023-05-03 15:29:05 +0200

    doing things

--- DAAP_HW2/colaCheck.m
+++ DAAP_HW2/colaCheck.m
@@ -12,3 +12,38 @@ figure
 
 plot(sig_istft)
 ylim([0.98 1.02])
+
+
+
+% fft check
+
+if not(isfolder("audioOutputs"))
+    mkdir("audioOutputs")
+end
+
+addpath('audioInputs')
+addpath('audioOutputs')
+
+y_1_name = "y1.wav";
+[y_1_t,y_1_Fs] = audioread(y_1_name);
+
+y_1_t = y_1_t(1:4096);
+
+
+y_1_w = fft(y_1_t);
+
+y_1_w_ang= angle(y_1_w);
+
+y_1_w_ang_1 = y_1_w_ang(1:2048,1);
+y_1_w_ang_2 = flip(y_1_w_ang(2049:end),1);
+
+figure
+subplot(2,1,1)
+plot(abs(y_1_w))
+subplot(2,1,2)
+plot(angle(y_1_w))
+
+figure
+plot(y_1_w_ang_1)
+hold on
+plot(-y_1_w_ang_2)
