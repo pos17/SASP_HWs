060c2fc2e68302067bcd015d1dfba00f8f3fd965
pox17 <paos9904@gmail.com>
2023-05-10 20:03:24 +0200

    added better plots to the code

--- SSSP_HW1/FractionalDelayLagInterp.m
+++ SSSP_HW1/FractionalDelayLagInterp.m
@@ -73,102 +73,186 @@ h_int_9 = LagrangeInterpolation(N,D);
 
 %% Lagrange interpolation plots
 
+lw = 1
+fontSize = 15
+figure('Renderer', 'painters', 'Position', [10 10 1000 1500])
 
-figure('Renderer', 'painters', 'Position', [10 10 1000 800])
-% stem(n_1, h_int_1),
-% hold on 
-% stem(n_2, h_int_2),
-% hold on 
-stem(n_4, h_int_4),
+subplot(3,2,1)
+stem(n_4, h_int_4,LineWidth=lw),
 hold on 
-stem(n_5, h_int_5),
+x = linspace(0,4,100);
+sinc_y = sinc(x-(D));
+plot(x,sinc_y,LineWidth=lw);
+xlabel('Samples',Interpreter="latex",FontSize=fontSize),
+ylabel('Impulse response',Interpreter="latex",FontSize=fontSize)
+xlim([-1 5])
+grid
+title('N = 4',Interpreter="latex",FontSize=fontSize)
+
+
+subplot(3,2,2)
+stem(n_5, h_int_5,LineWidth=lw),
+x = linspace(0,5,100);
+sinc_y = sinc(x-(D));
 hold on 
-stem(n_opt, h_int_opt),
+plot(x,sinc_y,LineWidth=lw);
+xlabel('Samples',Interpreter="latex",FontSize=fontSize),
+ylabel('Impulse response',Interpreter="latex",FontSize=fontSize)
+xlim([-1 6])
+grid
+
+title('N = 5',Interpreter="latex",FontSize=fontSize)
+
+
+subplot(3,2,3)
+stem(n_opt, h_int_opt,LineWidth=lw),
+x = linspace(0,6,100);
+sinc_y = sinc(x-(D));
 hold on 
-stem(n_7, h_int_7),
+plot(x,sinc_y,LineWidth=lw);
+xlabel('Samples',Interpreter="latex",FontSize=fontSize),
+ylabel('Impulse response',Interpreter="latex",FontSize=fontSize)
+xlim([-1 7])
+grid
+title('N = 6',Interpreter="latex",FontSize=fontSize)
+
+
+subplot(3,2,4)
+stem(n_7, h_int_7,LineWidth=lw),
+x = linspace(0,7,100);
+sinc_y = sinc(x-(D));
 hold on 
-stem(n_8, h_int_8),
+plot(x,sinc_y,LineWidth=lw);
+xlabel('Samples',Interpreter="latex",FontSize=fontSize),
+ylabel('Impulse response',Interpreter="latex",FontSize=fontSize)
+xlim([-1 8])
+grid
+title('N = 7',Interpreter="latex",FontSize=fontSize)
+
+
+subplot(3,2,5)
+stem(n_8, h_int_8,LineWidth=lw),
+x = linspace(0,8,100);
+sinc_y = sinc(x-(D));
 hold on 
-stem(n_9, h_int_9),
-xlabel('Samples',Interpreter="latex"),
-ylabel('Impulse response',Interpreter="latex")
-title('Lagrange interpolation',Interpreter="latex")
-%legend(["N=1","N=2","N=3","N= floor(2*D)","N=7","N= floor(4*D)","N= floor(5*D)"],'Location','northwest',Interpreter="latex");
-legend(["N=4","N=5","N=6","N=7","N=8","N=9"],'Location','northwest',Interpreter="latex");
+plot(x,sinc_y,LineWidth=lw);
+xlabel('Samples',Interpreter="latex",FontSize=fontSize),
+ylabel('Impulse response',Interpreter="latex",FontSize=fontSize)
+title('Lagrange interpolation',Interpreter="latex",FontSize=fontSize)
+xlim([-1 9])
+grid
+title('N = 8',Interpreter="latex",FontSize=fontSize)
+
+
+subplot(3,2,6)
+stem(n_9, h_int_9,LineWidth=lw),
+x = linspace(0,9,100);
+sinc_y = sinc(x-(D));
+hold on
+plot(x,sinc_y,LineWidth=lw);
+xlabel('Samples',Interpreter="latex",FontSize=fontSize),
+ylabel('Impulse response',Interpreter="latex",FontSize=fontSize)
+title('N = 9',Interpreter="latex",FontSize=fontSize)
+xlim([-1 10])
+grid
+sgtitle('Impulse response Comparison',Interpreter="latex",FontSize=fontSize+3);
 saveas(gcf,"Plots/FiltersIR.png")
 %[H_int, f] = freqz(h_int_1, 1, 'half', fs);
 
 figure('Renderer', 'painters', 'Position', [10 10 1000 1200])
 
 subplot(3,1,1)
-subtitle('Frequency response',Interpreter="latex");
+subtitle('Frequency response',Interpreter="latex",FontSize=fontSize);
 % plot(f_1*fs/pi/2, abs(H_int_1));
 % hold on
 % plot(f_2*fs/pi/2, abs(H_int_2));
 % hold on
 %norm_freq = (0,1,f_4*fs/pi/2);
-plot(f_4/(pi), abs(H_int_4));
+plot(f_4/(pi), abs(H_int_4),LineWidth=lw);
 hold on
-plot(f_5/(pi), abs(H_int_5));
+plot(f_5/(pi), abs(H_int_5),LineWidth=lw);
 hold on
-plot(f_opt/(pi), abs(H_int_opt));
+plot(f_opt/(pi), abs(H_int_opt),LineWidth=lw);
 hold on
-plot(f_7/(pi), abs(H_int_7));
+plot(f_7/(pi), abs(H_int_7),LineWidth=lw);
 hold on
-plot(f_8/(pi), abs(H_int_8));
+plot(f_8/(pi), abs(H_int_8),LineWidth=lw);
 hold on
-plot(f_9/(pi), abs(H_int_9));
+plot(f_9/(pi), abs(H_int_9),LineWidth=lw);
 ylim([0 1.7]);
 %xlim([0 fs/2]);
-xlabel('Frequency [Hz]',Interpreter="latex"), ylabel('Magnitude [dB]',Interpreter="latex");
+xlabel('Frequency [Hz]',Interpreter="latex",FontSize=fontSize), ylabel('Magnitude [dB]',Interpreter="latex",FontSize=fontSize);
 %legend(["N=1","N=2","N=3","N= floor(2*D)","N=7","N= floor(4*D)","N= floor(5*D)"],'Location','northwest',Interpreter="latex");
-legend(["N=4","N=5","N=6","N=7","N=8","N=9"],'Location','northwest',Interpreter="latex");
+legend(["N=4","N=5","N=6","N=7","N=8","N=9"],'Location','northwest',Interpreter="latex",FontSize=fontSize);
 hold on
 %axis([0, fs/2, 0 2]),
 grid
 
 subplot(3,1,2)
-subtitle('Group Delay',Interpreter="latex");
+subtitle('Group Delay',Interpreter="latex",FontSize=fontSize);
 % grpdelay(h_int_1, 1, 1024, 'half', fs);
 % hold on 
 % grpdelay(h_int_2, 1, 1024, 'half', fs);
 % hold on 
-grpdelay(h_int_4, 1, 1024);
+[gd,w]=grpdelay(h_int_4, 1, 1024);
+plot(w/pi,gd,LineWidth=lw)
+
 hold on
-grpdelay(h_int_5, 1, 1024);
+[gd,w]=grpdelay(h_int_5, 1, 1024);
+plot(w/pi,gd,LineWidth=lw)
+
 hold on 
-grpdelay(h_int_opt, 1, 1024);
+[gd,w]=grpdelay(h_int_opt, 1, 1024);
+plot(w/pi,gd,LineWidth=lw)
+
 hold on 
-grpdelay(h_int_7, 1, 1024);
+[gd,w]=grpdelay(h_int_7, 1, 1024);
+plot(w/pi,gd,LineWidth=lw)
+
 hold on 
-grpdelay(h_int_8, 1, 1024);
+[gd,w]=grpdelay(h_int_8, 1, 1024);
+plot(w/pi,gd,LineWidth=lw)
+
 hold on 
-grpdelay(h_int_9, 1, 1024);
+[gd,w]=grpdelay(h_int_9, 1, 1024);
+plot(w/pi,gd,LineWidth=lw)
+
 ylim([1 4]);
-xlabel('Normalized Frequency $(x \pi [rad/sample])$',Interpreter="latex"), ylabel('Group Delay [samples]',Interpreter="latex");
+xlabel('Normalized Frequency $(x \pi [rad/sample])$',Interpreter="latex",FontSize=fontSize), ylabel('Group Delay [samples]',Interpreter="latex",FontSize=fontSize);
 %xlim([0 fs/2]);
 %legend(["N=1","N=2","N=3","N= floor(2*D)","N=7","N= floor(4*D)","N= floor(5*D)"],'Location','northwest',Interpreter="latex");
-legend(["N=4","N=5","N=6","N=7","N=8","N=9"],'Location','northwest',Interpreter="latex");
-
+legend(["N=4","N=5","N=6","N=7","N=8","N=9"],'Location','northwest',Interpreter="latex",FontSize=fontSize);
+grid
 subplot(3,1,3)
-subtitle('Group Delay',Interpreter="latex");
-phasedelay(h_int_4, 1, 1024);
-hold on 
-phasedelay(h_int_5, 1, 1024);
-hold on 
-phasedelay(h_int_opt, 1, 1024);
-hold on 
-phasedelay(h_int_7, 1, 1024);
-hold on 
-phasedelay(h_int_8, 1, 1024);
-hold on 
-phasedelay(h_int_9, 1, 1024);
-xlabel('Normalized Frequency $(x \pi [rad/sample])$',Interpreter="latex"), ylabel('Phase Delay [samples]',Interpreter="latex");
+subtitle('Phase Delay',Interpreter="latex",FontSize=fontSize);
+[pd,w]=phasedelay(h_int_4, 1, 1024);
+plot(w/pi,pd,LineWidth=lw)
 
+hold on 
+[pd,w]=phasedelay(h_int_5, 1, 1024);
+plot(w/pi,pd,LineWidth=lw)
+
+hold on 
+[pd,w]=phasedelay(h_int_opt, 1, 1024);
+plot(w/pi,pd,LineWidth=lw)
+
+hold on 
+[pd,w]=phasedelay(h_int_7, 1, 1024);
+plot(w/pi,pd,LineWidth=lw)
+
+hold on 
+[pd,w]=phasedelay(h_int_8, 1, 1024);
+plot(w/pi,pd,LineWidth=lw)
+
+hold on 
+[pd,w]=phasedelay(h_int_9, 1, 1024);
+plot(w/pi,pd,LineWidth=lw)
+xlabel('Normalized Frequency $(x \pi [rad/sample])$',Interpreter="latex",FontSize=fontSize), ylabel('Phase Delay [samples]',Interpreter="latex",FontSize=fontSize);
+grid
 %xlim([0 1024]);
 %legend(["N=1","N=2","N=3","N= floor(2*D)","N=7","N= floor(4*D)","N= floor(5*D)"],'Location','northwest',Interpreter="latex");
-legend(["N=4","N=5","N=6","N=7","N=8","N=9"],'Location','northwest',Interpreter="latex");
-sgtitle('Filter response orders comparison',Interpreter="latex");
+legend(["N=4","N=5","N=6","N=7","N=8","N=9"],'Location','northwest',Interpreter="latex",FontSize=fontSize);
+sgtitle('Filter response orders comparison',Interpreter="latex",FontSize=fontSize);
 saveas(gcf,"Plots/FreqResponseComparison.png")
 
 
