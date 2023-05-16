4e4697ffee01979077106fef2e1585815f190ec4
pox17 <paos9904@gmail.com>
2023-05-16 14:27:35 +0200

    updated to start HW4

---
+++ SSSP_HW4/HW4/HW4CrossoverNetWD.m
@@ -0,0 +1,120 @@
+clear all
+close all
+clc
+
+%% Import Input Audio Signal
+[Vin,~] = audioread('ExpSweep.wav');
+
+%% LTSpice Files for Ground-Truth
+[OutLowSpice,~]=audioread('outlowsweep.wav');
+[OutMidSpice,~]=audioread('outmidsweep.wav');
+[OutHighSpice,FsLTSpice]=audioread('outhighsweep.wav');
+TsLTSpice=1/FsLTSpice;
+
+%% Sampling frequency (to be varied: FsLTSpice/downSampFact, with downSampFact={4,3,2})
+downSampFact=2;
+Fs =FsLTSpice/downSampFact; 
+
+%% Downsample Input Signal
+Vin=Vin([1:downSampFact:end]);
+
+%% Sampling Period
+Ts=1/Fs;
+%% Number of Samples
+Nsamp=length(Vin);
+%% Simulated time
+tstop=Nsamp*Ts;
+%% Parameters of Dynamic Element
+L1=0.35*10^(-3);
+L2=0.35*10^(-3);
+L3=3.5*10^(-3);
+L4=3.5*10^(-3);
+C1= 2.8*10^(-6);
+C2= 2.8*10^(-6);
+C3= 28*10^(-6);
+C4= 4.7*10^(-6);
+C5=28*10^(-6);
+C6=47*10^(-6);
+%% Resistive Parameters
+R1=10;
+RspkLow=8;
+R2=10;
+RspkMid=8;
+RspkHigh=8;
+%% WDF setting of free parameters (adaptation conditions)
+
+
+%% Computation of Scattering Matrices
+
+
+%% Initialization of Waves
+
+
+%% Initialize Output Signals
+% Low
+VoutLow=zeros(size(Vin));
+% Mid
+VoutMid=zeros(size(Vin));
+% High
+VoutHigh=zeros(size(Vin));
+
+ii=0;
+while (ii<Nsamp)
+    ii=ii+1;
+
+    %% Manage Dynamic Elements
+            
+    
+    %% Forward Scan
+
+
+    %% Local Root Scattering
+
+
+    %% Backward Scan
+
+
+    %% Read Output
+  
+    
+end
+
+
+%% Output Plots
+figure
+set(gcf, 'Color', 'w');
+subplot(311)
+plot(TsLTSpice*[1:length(OutLowSpice)],OutLowSpice,'r','Linewidth',2); hold on;
+plot(Ts*[1:Nsamp],VoutLow,'b--','Linewidth',1); grid on; xlim([0,tstop]); 
+xlabel('time [seconds]','Fontsize',16,'interpreter','latex');
+ylabel('$V_{\mathrm{outLow}}$ [V]','Fontsize',16,'interpreter','latex');
+legend('LTspice','WDF','Fontsize',16,'interpreter','latex');
+title('Output Signals','Fontsize',18,'interpreter','latex');
+subplot(312)
+plot(TsLTSpice*[1:length(OutMidSpice)],OutMidSpice,'r','Linewidth',2); hold on;
+plot(Ts*[1:Nsamp],VoutMid,'b--','Linewidth',1); grid on; xlim([0,tstop]);
+xlabel('time [seconds]','Fontsize',16,'interpreter','latex');
+ylabel('$V_{\mathrm{outMid}}$ [V]','Fontsize',16,'interpreter','latex');
+subplot(313)
+plot(TsLTSpice*[1:length(OutHighSpice)],OutHighSpice,'r','Linewidth',2); hold on;
+plot(Ts*[1:Nsamp],VoutHigh,'b--','Linewidth',1); grid on; xlim([0,tstop]);
+xlabel('time [seconds]','Fontsize',16,'interpreter','latex');
+ylabel('$V_{\mathrm{outHigh}}$ [V]','Fontsize',16,'interpreter','latex');
+
+%% Error Plots
+figure
+set(gcf, 'Color', 'w');
+subplot(311)
+plot(Ts*[1:Nsamp],OutLowSpice([1:downSampFact:end])-VoutLow,'k','Linewidth',1);grid on; xlim([0,tstop]);
+xlabel('time [seconds]','Fontsize',16,'interpreter','latex');
+ylabel('$E_{\mathrm{outLow}}$ [V]','Fontsize',16,'interpreter','latex');
+title(['Error Signals. $F_{\mathrm{s}}=$ ',num2str(Fs),' Hz'],'Fontsize',18,'interpreter','latex');
+subplot(312)
+plot(Ts*[1:Nsamp],OutMidSpice([1:downSampFact:end])-VoutMid,'k','Linewidth',1);grid on; xlim([0,tstop]);
+xlabel('time [seconds]','Fontsize',16,'interpreter','latex');
+ylabel('$E_{\mathrm{outMid}}$ [V]','Fontsize',16,'interpreter','latex');
+subplot(313)
+plot(Ts*[1:Nsamp],OutHighSpice([1:downSampFact:end])-VoutHigh,'k','Linewidth',1);grid on; xlim([0,tstop]);
+xlabel('time [seconds]','Fontsize',16,'interpreter','latex');
+ylabel('$E_{\mathrm{outHigh}}$ [V]','Fontsize',16,'interpreter','latex');
+
